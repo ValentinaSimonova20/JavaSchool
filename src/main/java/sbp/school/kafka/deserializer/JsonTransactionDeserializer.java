9485b0caa411802f27eb3dd8ccbc311c8022c8ae
valentina <simonova.valentina19@gmail.com>
2024-12-30 17:52:42 +0300

    исправления по ревью

--- src/main/java/sbp/school/kafka/deserializer/JsonTransactionDeserializer.java
+++ src/main/java/sbp/school/kafka/deserializer/JsonTransactionDeserializer.java
@@ -2,14 +2,19 @@ package sbp.school.kafka.deserializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 import sbp.school.kafka.util.Transaction;
 import sbp.school.kafka.validation.ValidationService;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 
 public class JsonTransactionDeserializer implements Deserializer<Transaction> {
+
+    private static final Logger logger = Logger.getLogger(JsonTransactionDeserializer.class.getName());
+
     @Override
     public Transaction deserialize(String topic, byte[] data) {
         if(data != null) {
@@ -20,10 +25,10 @@ public class JsonTransactionDeserializer implements Deserializer<Transaction> {
                 ValidationService.validateBySchema(objectMapper.readTree(data));
                 return objectMapper.readValue(data, Transaction.class);
             } catch (IOException e) {
-                System.out.println("JsonProcessingException: " + e.getMessage());
-                throw new RuntimeException(e);
+                logger.severe("JsonProcessingException: " + e.getMessage());
+                throw new SerializationException(e);
             }
         }
-        throw new RuntimeException("Получили null в значении");
+        throw new SerializationException("Не получилось десериализовать значение потому что оно null");
     }
 }
